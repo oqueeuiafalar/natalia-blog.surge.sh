eed2742ddcfafbfdc799a741ff4ae7f02c2638fc
Jota Teles <josetelesmaciel@gmail.com>
2017-02-21 23:16:58 -0300

    injeta novos modulos de widgets adicionados

--- oquequeeuiafalar.module.js
+++ oquequeeuiafalar.module.js
@@ -4,6 +4,11 @@
 	angular.module("oquequeeuiafalar", ["frontpress", 
 		"oquequeeuiafalar.components.header-navigator", 
 		"oquequeeuiafalar.components.header-social-links",
-		"oquequeeuiafalar.components.widgets.author-information"]);
+		"oquequeeuiafalar.components.widgets.author-information",
+		"oquequeeuiafalar.components.widgets.related-posts",
+		"oquequeeuiafalar.components.widgets.publicidade-sidebar",
+		"oquequeeuiafalar.components.widgets.instagram",
+		"oquequeeuiafalar.components.widgets.links-institucionais",
+		"oquequeeuiafalar.components.widgets.related-posts"]);
 	
 })();
